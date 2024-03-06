029360f5beb49695462c64d7f34994dbf662337d
Artyom Klyuev <artemkluev2000@yandex.ru>
2024-03-06 22:04:21 +0500

    predeploy done-5

--- src/main.ts
+++ src/main.ts
@@ -22,8 +22,8 @@ async function start() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    // origin: 'http://localhost',
-    origin: 'http://51.250.90.55', 
+    origin: 'http://localhost',
+    // origin: 'http://51.250.90.55',
     credentials: true,
   });
 
